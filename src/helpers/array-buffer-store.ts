62df20ac5ec3f4197cc02683b897b0430af72f73
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-10 07:24:30 +0100

    feat: validate values for begin and end

--- src/helpers/array-buffer-store.ts
+++ src/helpers/array-buffer-store.ts
@@ -31,7 +31,19 @@ export class ArrayBufferStore {
             throw new Error(`There is no arrayBuffer stored with an id called "${ id }".`);
         }
 
-        // @todo Validate the values of begin and end.
+        if (begin < 0 || begin > arrayBuffer.byteLength) {
+            throw new Error(`The given value for begin "${ begin }" is out of bounds.`);
+        }
+
+        if (end !== null) {
+            if (end > arrayBuffer.byteLength) {
+                throw new Error(`The given value for end "${ end }" is out of bounds.`);
+            }
+
+            if (end < begin) {
+                throw new Error(`The given value for end "${ end }" is below the given value for begin "${ begin }".`);
+            }
+        }
 
         return arrayBuffer.slice(begin, (end === null) ? arrayBuffer.byteLength : end);
     }
