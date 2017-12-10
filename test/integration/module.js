2a98a0b05af9656b2c417e988ff61922fc81afce
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-10 16:35:45 +0100

    test: add missing integration tests

--- test/integration/module.js
+++ test/integration/module.js
@@ -265,7 +265,170 @@ describe('module', () => {
 
     describe('slice()', () => {
 
-        // @todo
+        let arrayBufferId;
+        let id;
+
+        beforeEach(() => {
+            arrayBufferId = 17;
+            id = 43;
+        });
+
+        describe('without a stored arrayBuffer', () => {
+
+            it('should return an error', (done) => {
+                worker.addEventListener('message', ({ data }) => {
+                    expect(data).to.deep.equal({
+                        error: {
+                            message: `There is no arrayBuffer stored with an id called "${ arrayBufferId }".`
+                        },
+                        id,
+                        result: null
+                    });
+
+                    done();
+                });
+
+                worker.postMessage({ id, method: 'slice', params: { arrayBufferId } });
+            });
+
+        });
+
+        describe('with a stored arrayBuffer', () => {
+
+            let values;
+
+            beforeEach((done) => {
+                values = [ Math.random(), Math.random(), Math.random() ];
+
+                const float64Array = new Float64Array(values);
+                const onMessage = () => {
+                    worker.removeEventListener('message', onMessage);
+
+                    done();
+                };
+
+                worker.addEventListener('message', onMessage);
+                worker.postMessage({ id, method: 'store', params: { arrayBuffer: float64Array.buffer, arrayBufferId } }, [ float64Array.buffer ]);
+            });
+
+            it('should return the cloned arrayBuffer with the given id', (done) => {
+                worker.addEventListener('message', ({ data }) => {
+                    const float64Array = new Float64Array(data.result.arrayBuffer);
+
+                    expect(Array.from(float64Array)).to.deep.equal(values);
+
+                    expect(data).to.deep.equal({
+                        error: null,
+                        id,
+                        result: { arrayBuffer: data.result.arrayBuffer }
+                    });
+
+                    done();
+                });
+
+                worker.postMessage({ id, method: 'slice', params: { arrayBufferId, begin: 0 } });
+            });
+
+            it('should return the sliced arrayBuffer with the given id', (done) => {
+                worker.addEventListener('message', ({ data }) => {
+                    const float64Array = new Float64Array(data.result.arrayBuffer);
+
+                    expect(Array.from(float64Array)).to.deep.equal(values.slice(1, 2));
+
+                    expect(data).to.deep.equal({
+                        error: null,
+                        id,
+                        result: { arrayBuffer: data.result.arrayBuffer }
+                    });
+
+                    done();
+                });
+
+                worker.postMessage({ id, method: 'slice', params: { arrayBufferId, begin: Float64Array.BYTES_PER_ELEMENT, end: Float64Array.BYTES_PER_ELEMENT * 2 } });
+            });
+
+            describe('with a value for begin below zero', () => {
+
+                it('should return an error', (done) => {
+                    worker.addEventListener('message', ({ data }) => {
+                        expect(data).to.deep.equal({
+                            error: {
+                                message: 'The given value for begin "-1" is out of bounds.'
+                            },
+                            id,
+                            result: null
+                        });
+
+                        done();
+                    });
+
+                    worker.postMessage({ id, method: 'slice', params: { arrayBufferId, begin: -1 } });
+                });
+
+            });
+
+            describe('with a value for begin above the size of the arrayBuffer', () => {
+
+                it('should return an error', (done) => {
+                    worker.addEventListener('message', ({ data }) => {
+                        expect(data).to.deep.equal({
+                            error: {
+                                message: 'The given value for begin "25" is out of bounds.'
+                            },
+                            id,
+                            result: null
+                        });
+
+                        done();
+                    });
+
+                    worker.postMessage({ id, method: 'slice', params: { arrayBufferId, begin: (Float64Array.BYTES_PER_ELEMENT * 3) + 1 } });
+                });
+
+            });
+
+            describe('with a value for end above the size of the arrayBuffer', () => {
+
+                it('should return an error', (done) => {
+                    worker.addEventListener('message', ({ data }) => {
+                        expect(data).to.deep.equal({
+                            error: {
+                                message: 'The given value for end "25" is out of bounds.'
+                            },
+                            id,
+                            result: null
+                        });
+
+                        done();
+                    });
+
+                    worker.postMessage({ id, method: 'slice', params: { arrayBufferId, begin: 0, end: (Float64Array.BYTES_PER_ELEMENT * 3) + 1 } });
+                });
+
+            });
+
+            describe('with a value for end below the value of begin', () => {
+
+                it('should return an error', (done) => {
+                    worker.addEventListener('message', ({ data }) => {
+                        expect(data).to.deep.equal({
+                            error: {
+                                message: 'The given value for end "4" is below the given value for begin "12".'
+                            },
+                            id,
+                            result: null
+                        });
+
+                        done();
+                    });
+
+                    worker.postMessage({ id, method: 'slice', params: { arrayBufferId, begin: 12, end: 4 } });
+                });
+
+            });
+
+
+        });
 
     });
 
