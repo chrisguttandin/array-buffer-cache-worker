323f1aead3695c3ac68bb6ce0f9e05ca75d93574
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-01-27 19:30:39 +0100

    test: run karma tests with the cli

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -4,18 +4,14 @@ module.exports = {
         'sh:build-es2018',
         'sh:build-es5'
     ],
-    continuous: [
-        // @todo This is broken now.
-        'karma:continuous'
-    ],
     lint: [
         'eslint',
         // @todo Use grunt-lint again when it support the type-check option.
         'sh:lint'
     ],
     test: [
-        'karma:test-integration',
-        'karma:test-unit',
-        'karma:expectation'
+        'sh:test-expectation',
+        'sh:test-integration',
+        'sh:test-unit'
     ]
 };
