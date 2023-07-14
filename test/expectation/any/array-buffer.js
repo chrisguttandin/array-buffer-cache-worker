34b1ae75ab0f9c20872c549b203960fb97dfdd80
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-07-14 15:17:28 +0200

    test: tweak magic values

--- test/expectation/any/array-buffer.js
+++ test/expectation/any/array-buffer.js
@@ -14,9 +14,9 @@ describe('ArrayBuffer', () => {
 
             /*
              * 2147479551 is the largest possible length but an ArrayBuffer of that size can't be sliced by Chrome and Safari on
-             * Sauce Labs. However a size of 200,000,000 bytes is enough to let the test pass.
+             * Sauce Labs. However a size of 300,000,000 bytes is enough to let the test pass.
              */
-            arrayBuffer = new ArrayBuffer(200000000);
+            arrayBuffer = new ArrayBuffer(300000000);
         });
 
         it('should block the main thread', function () {
@@ -25,14 +25,14 @@ describe('ArrayBuffer', () => {
             let lastNow = performance.now();
             let currentNow = performance.now();
 
-            expect(currentNow - lastNow).to.be.below(90);
+            expect(currentNow - lastNow).to.be.below(30);
 
             arrayBuffer.slice(0);
 
             lastNow = currentNow;
             currentNow = performance.now();
 
-            expect(currentNow - lastNow).to.be.above(90);
+            expect(currentNow - lastNow).to.be.above(30);
         });
     });
 });
